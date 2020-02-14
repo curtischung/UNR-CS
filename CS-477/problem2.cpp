2ef95f5e82294d9cc68ddb0272377fe8dba96134
AgentTripleCCC <agenttripleccc@gmail.com>
2020-02-14 01:28:59 -0800

    attempt to actually solve real homework problem

--- CS-477/problem2.cpp
+++ CS-477/problem2.cpp
@@ -11,84 +11,49 @@
 
 using namespace std;
 
-void merge(int arr[], int left, int mid, int right);
-void mergeSort(int arr[], int left, int right);
+
+int MaxArrayIndex (int arr[], int left, int right) {
+	int mid;
+	int maxLeft = 0;
+	int maxRight = 0;
+
+	if (arr[left] >= arr[right])
+	{
+		return left;
+	}
+	else
+	{
+		mid = left + (right - left) / 2;
+		cout << maxLeft << " ";
+		maxLeft = MaxArrayIndex(arr, left, mid);
+		cout << endl;
+		cout << maxRight << " ";
+		maxRight = MaxArrayIndex(arr, mid + 1, right);
+
+		if (arr[maxLeft] >= arr[maxRight])
+		{
+			return maxLeft;
+		}
+		else
+		{
+			return maxRight;
+		}
+
+	}
+}
 void printArray(int arr[]);
 
 int main (int argc, char* argv[]) 
 {
 	int A[] = {1, 4, 9, 3, 4, 9, 5, 6, 9, 3 ,7};
+	int result;
 
-	cout << "Before merge sort: ";
-	printArray(A);
-	cout << endl;
-	mergeSort(A, 0, SIZE);
-	cout << "After merge sort: ";
-	printArray(A);
+
+	result = MaxArrayIndex(A, 0, SIZE);
+	cout << "Largest number position: " << result << endl;
 }
 
-void merge(int arr[], int left, int mid, int right) 
-{
-	int i, j, k;
-	int n1 = mid - left + 1;
-	int n2 = right - mid;
 
-	int L[n1], R[n2];
-
-	for (i = 0; i < n1; i++) {
-		L[i] = arr[left + i];
-	}
-	for (j = 0; j < n2; j++) {
-		R[j] = arr[mid + 1 + j];
-	}
-
-	i = 0;
-	j = 0;
-	k = left;
-
-	while (i < n1 && j < n2)
-	{
-		if (L[i] <= R[j]) 
-		{
-			arr[k] = L[i];
-			i++;
-		} 
-		else 
-		{
-			arr[k] = R[j];
-			j++;
-		}
-		k++;
-	}
-
-	while (i < n1) 
-	{
-		arr[k] = L[i];
-		i++;
-		k++;
-	}
-
-	while (j < n2)
-	{
-		arr[k] = R[j];
-		j++;
-		k++;
-	}
-}
-
-void mergeSort(int arr[], int left, int right) 
-{ 
-    if (left < right) 
-    { 
-        int mid = left+(right-left) / 2; 
-  
-      
-        mergeSort(arr, left, mid); 
-        mergeSort(arr, mid + 1, right); 
-  
-        merge(arr, left, mid, right); 
-    } 
-}
 void printArray(int arr[]) {
 	for(int i = 0; i < SIZE; i++) {
 		cout << arr[i] << " ";
